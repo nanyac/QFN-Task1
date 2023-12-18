9d36ca7e50ffc3907af624b3255363cf0ae72361
denismedvedev478 <57592135+denismedvedev478@users.noreply.github.com>
2023-12-18 19:46:18 +0300

    Update chunk_utils.py

--- chunk_utils.py
+++ chunk_utils.py
@@ -1,16 +1,54 @@
-global Vgs_Quiescent
-Vgs_Quiescent = -2.20332E-03 
+Vgs_Quiescent = -2.20332E-03
+def setGlobalVgsQuiescent(var:float) -> None:   # Метод-обёртка для изменения Vgs_Quiescent
+    global Vgs_Quiescent 
+    Vgs_Quiescent = var
+def getGlobalVgsQuiescent() -> float:           # Метод-обёртка для получения Vgs_Quiescent
+    return Vgs_Quiescent
 
-global Ig_Quiescent
-Ig_Quiescent=-1.12913E-07 
 
-global Vds_Quiescent
-Vds_Quiescent=+5.11725E-03 
+Ig_Quiescent = -1.12913E-07 
+def setGlobaIgQuiescent(var: float) -> None:
+    global Ig_Quiescent
+    Ig_Quiescent = var
+def getGlobaIgQuiescent() -> float:
+    return Ig_Quiescent
 
-global Id_Quiescent
-Id_Quiescent=+2.43304E-04
+
+Vds_Quiescent = +5.11725E-03 
+def setGlobaVdsQuiescent(var: float) -> None:
+    global Vds_Quiescent
+    Vds_Quiescent = var
+def getGlobaVdsQuiescent() -> float:
+    return Vds_Quiescent
+
+
+Id_Quiescent = +2.43304E-04
+def setGlobaIdQuiescent(var: float) -> None:
+    global Id_Quiescent
+    Id_Quiescent = var
+def getGlobaIdQuiescent() -> float:
+    return Id_Quiescent
+
+
+def setAllGlobals(_Vgs_Quiescent, _Ig_Quiescent, _Vds_Quiescent, _Id_Quiescent)->None:
+    global Vgs_Quiescent 
+    Vgs_Quiescent = _Vgs_Quiescent
+    global Ig_Quiescent
+    Ig_Quiescent = _Ig_Quiescent
+    global Vds_Quiescent
+    Vds_Quiescent = _Vds_Quiescent
+    global Id_Quiescent
+    Id_Quiescent = _Id_Quiescent
+
+
+def printAllGlobals() -> None:
+    print(f"Vgs_Quiescent = {Vgs_Quiescent}\nIg_Quiescent = {Ig_Quiescent}\nVds_Quiescent = {Vds_Quiescent}\nId_Quiescent = {Id_Quiescent}")
+
+
+
 
 class ChunkIn:
+    """Входной блок данных"""
     _Vgs = 0
     _Ig = 0
     _Vds = 0
@@ -33,10 +71,11 @@ class ChunkIn:
         deltaId = Id_Quiescent - self._Id
         return [deltaVgs, deltaIg, deltaVds, deltaId]
     
-    
+
     def getDeltaVgs(self) -> int:
-        """Vgs_Quiescent Vgs_Pulsated"""
-        deltaVgs = Vgs_Quiescent - self._Vgs
+        """Возвращает разность ТЕПЕРЬ Vgs_pulsed и Vgs_quistient"""
+        # self._Vgs - Pulsed Point Vgs
+        deltaVgs = self._Vgs - Vgs_Quiescent
         return deltaVgs
     
 
@@ -53,8 +92,46 @@ class ChunkIn:
 
 
 class ChunkOut:
+    """Результирующий блок данных"""
     _freq = 0
     _s_dVgs = dict()
     def __init__(self, freq: float) -> None:
         self._freq = freq
 
+
+class Task2:
+    a1 = 0.0
+    a2 = 0.0
+    b1 = 0.0
+    b2 = 0.0
+    Zs = 0.0
+    Zin = 0.0
+    
+    def __init__(self, a1, a2, b1, b2, Zs, Zin):
+        self.a1 = a1
+        self.a2 = a2
+        self.b1 = b1
+        self.b2 = b2
+        self.Zs = Zs
+        self.Zin = Zin
+    
+        self.Pin_delivered = 1/2*(a1**2 - b1**2)
+        
+        self.Pin_available = self.Pin_delivered/(1 - 
+                                pow((Zin - Zs)/(Zin + Zs), 2))
+
+        self.Pout = 1/2*(b2**2 - a2**2)
+
+        self.Pdc = 1j   #pdc = v_out* i_out?????????
+        
+        self.PAE = (self.Pout - self.Pin_delivered)/self.Pdc
+
+        self.transduser_eff = (self.Pout - self.Pin_delivered)/self.Pdc
+
+        self.Deff = self.Pout/self.Pdc
+
+        self.Gp = self.Pout/self.Pin_delivered
+        
+        self.Gt = self.Pout/self.Pin_available
+        
+        self.PowerGain = self.Pout/self.Pin_delivered
